0976d23843898d5cf8a954dc6a2e84e9bf2e6de9
bmacc <bdacosta1@student.gsu.edu>
2013-07-22 02:43:26 -0400

    Create registrationII.php

---
+++ registrationII.php
@@ -0,0 +1,20 @@
+<div id="members_welcome">
+  <div id="index_top">
+		<div id="members_login">
+			<?php	
+				echo "<a class='a1' href=login.php>Login</a>";
+			?>			
+			<div id="members_main">
+				<?php
+					echo "<a class='a1' href=index.php>Main Page</a>";		
+				?>
+			</div>
+		</div>		
+	</div>
+	<div id="your_home">
+		<?php							
+			echo "Welcome to The Archives";
+		?>
+	</div>
+</div>
+
