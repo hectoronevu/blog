05c7e4cdfe953b1c3d368d0b9c3d3c0039e8c242
bmacc <bdacosta1@student.gsu.edu>
2013-07-22 01:24:08 -0400

    Update commentsII.php

--- commentsII.php
+++ commentsII.php
@@ -1,35 +1,33 @@
-<html>
-<head>
-</head>
-<body>
-
-  <div id="logout_widget">
+<div id="logout_widget">
 	<?php
-		echo "Welcome " . $username . "&nbsp&nbsp&nbsp<a href=logout.php class='a1'>Logout</a>";
+		echo "Welcome " . "<a class='a1' href=members.php>$username</a>" . "&nbsp&nbsp&nbsp<a href=logout.php class='a1'>Logout</a>";
 	?>
-	</div>
-	<div id="main_page">
-		<?php
-			echo "<a href=index.php class='a1'>Main page</a> <br />";
-		?>
-	</div>
-	<div id="archives">
-		<?php
+</div>
+
+<div id="main_page">
+	<?php
+		echo "<a href=index.php class='a1'>Main page</a> <br />";
+	?>
+</div>
+
+<div id="archives">
+	<?php
 		echo "Archives";									
-		?>
-	</div>
+	?>
+</div>
 	
-	<div id="comment_block">
-		<?php
-		$posts = mysql_query("SELECT * FROM posts WHERE postID = '$_GET[postID]'");
-                    while ($row = mysql_fetch_array($posts)) {
-                        echo $row['title'];
-                        echo " on " . $row['date'];
-                        echo "<br />";
-                        echo $row['content'];
-                        echo "<br /><br />";
-                    }
+<div id="comment_block">
+
+
+	<?php
+	error_reporting(0);
+	$posts = mysql_query("SELECT * FROM posts WHERE postID = '$_GET[postID]'");
+		 while ($row = mysql_fetch_array($posts)) {
+			 echo $row['title'];
+			 echo " on " . $row['date'];
+			 echo "<br />";
+			 echo $row['content'];
+			 echo "<br /><br />";
+			 }
 		?>
-	</div>
-</body>
-</html>
+</div>
