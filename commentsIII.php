d9b3dc091125f619a6ff310c677512f203262bc7
bmacc <bdacosta1@student.gsu.edu>
2013-07-21 21:19:00 -0400

    Create commentsIII.php

---
+++ commentsIII.php
@@ -0,0 +1,10 @@
+<div id="posted_comments">
+  <?php
+		echo $row['author'];
+		echo " on " . $row['date'];
+		echo "<br />";
+		echo $row['content'];
+		echo "<br />";
+		echo "<br />";
+	?>
+</div>
