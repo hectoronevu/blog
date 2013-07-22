ff4486ffb2e783e53ee54d255f47f695e26ed7ce
hectoronevu <hectoronevu@gmail.com>
2013-07-22 02:13:46 -0400

    Update pages.php
    added feature that will show how many comments there are for each post

--- pages.php
+++ pages.php
@@ -18,6 +18,18 @@ if (isset($_COOKIE['ID_my_site'])) {
             echo "Welcome " . $username . "<p>";
             echo "<a href=index.php>Main page</a> <br />";
             echo "<a href=logout.php>Logout</a> <br /> <br />";
+//            $comment= mysql_query("SELECT * FROM comments WHERE blogID = '$_GET[blogID]'");
+//            while ($row = mysql_fetch_array($comment)) {
+//                if (isset($_COOKIE['ID_my_site'])) {
+//                    echo "<a href=comments.php?postID=" . $row['postID'] . ">" . $row['title'] . "</a>";
+//                } else {
+//                    echo $row['title'];
+//                }
+//                echo " on " . $row['date'];
+//                echo "<br />";
+//                echo $row['content'];
+//                echo "<br /><br />";
+//            }
             $result = mysql_query("SELECT * FROM posts WHERE blogID = '$_GET[blogID]'");
             while ($row = mysql_fetch_array($result)) {
                 if (isset($_COOKIE['ID_my_site'])) {
@@ -25,8 +37,14 @@ if (isset($_COOKIE['ID_my_site'])) {
                 } else {
                     echo $row['title'];
                 }
-//                echo $row['title'];
                 echo " on " . $row['date'];
+                $post = $row['postID'];
+                $counter = 0;
+                $comment = mysql_query("SELECT * FROM comments WHERE postID = '$post'");
+                while ($cur = mysql_fetch_array($comment)) {
+                    $counter++;
+                }
+                echo "with " . $counter . " comments";
                 echo "<br />";
                 echo $row['content'];
                 echo "<br /><br />";
