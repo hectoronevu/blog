c0fca74bcb034917b981d59ca8328ebbf28081be
bmacc <bdacosta1@student.gsu.edu>
2013-07-22 00:40:49 -0400

    Create membersII.php

---
+++ membersII.php
@@ -0,0 +1,22 @@
+
+<div id="members_welcome">
+  <div id="index_top">
+		<div id="members_login">			
+			<?php
+				echo "Welcome " . $username ."&nbsp&nbsp&nbsp<a class='a1' href=logout.php>Logout</a>";		
+			?>
+			<div id="members_main">
+				<?php
+					echo "<a class='a1' href=index.php>Main page</a>";		
+				?>
+			</div>
+		</div>		
+	</div>
+	<div id="your_home">
+		<?php							
+			echo "Your Home:";
+		?>
+	</div>
+</div>
+
+
